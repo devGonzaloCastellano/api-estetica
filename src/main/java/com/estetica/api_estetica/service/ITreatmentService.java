e37c9945042670fe3d1c00cb65f4514a22fe40de
devGonzaloCastellano <devgonzalocastellano@gmail.com>
2026-02-15 13:56:52 -0300

    refactor(treatment): update interface with new DTOs

--- src/main/java/com/estetica/api_estetica/service/ITreatmentService.java
+++ src/main/java/com/estetica/api_estetica/service/ITreatmentService.java
@@ -1,13 +1,15 @@
 package com.estetica.api_estetica.service;
 
-import com.estetica.api_estetica.dto.treatment.TreatmentDTO;
+import com.estetica.api_estetica.dto.treatment.TreatmentCreateDTO;
+import com.estetica.api_estetica.dto.treatment.TreatmentResponseDTO;
+import com.estetica.api_estetica.dto.treatment.TreatmentUpdateDTO;
 
 import java.util.List;
 
 public interface ITreatmentService {
 
-    List<TreatmentDTO> getTreatment();
-    TreatmentDTO createTreatment(TreatmentDTO treatmentDto);
-    TreatmentDTO updateTreatmen(Long id, TreatmentDTO treatmentDto);
+    List<TreatmentResponseDTO> getTreatment();
+    TreatmentResponseDTO createTreatment(TreatmentCreateDTO dto);
+    TreatmentResponseDTO updateTreatmen(Long id, TreatmentUpdateDTO dto);
     void deleteTreatment(Long id);
 }

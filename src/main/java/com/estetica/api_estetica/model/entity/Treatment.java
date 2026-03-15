049ffb1d9835fee83dd60f8d66da5facd30eff88
devGonzaloCastellano <devgonzalocastellano@gmail.com>
2026-03-15 13:43:52 -0300

    feat: add @Table annotation to Appointment and Treatment entities

--- src/main/java/com/estetica/api_estetica/model/entity/Treatment.java
+++ src/main/java/com/estetica/api_estetica/model/entity/Treatment.java
@@ -1,9 +1,6 @@
 package com.estetica.api_estetica.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter @Setter
@@ -11,6 +8,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "treatments")
 public class Treatment {
 
     @Id

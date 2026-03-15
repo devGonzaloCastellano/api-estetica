e230acc12185675677b2245defb7e92aa9dfb7e9
devGonzaloCastellano <devgonzalocastellano@gmail.com>
2026-03-15 13:28:56 -0300

    feat: add Auth controller for security

---
+++ src/main/java/com/estetica/api_estetica/controller/AuthenticationController.java
@@ -0,0 +1,26 @@
+package com.estetica.api_estetica.controller;
+
+import com.estetica.api_estetica.dto.AuthLoginRequestDTO;
+import com.estetica.api_estetica.service.UserDetailsServiceImp;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private UserDetailsServiceImp userDetailsService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthLoginRequestDTO userRequest){
+        return new ResponseEntity<>(this.userDetailsService.loginUser(userRequest), HttpStatus.OK);
+    }
+}

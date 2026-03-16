a91dd69ab48ce814f0cc3395347d18dc84b54500
devGonzaloCastellano <devgonzalocastellano@gmail.com>
2026-03-16 12:15:55 -0300

    feat: add new method for find username

--- src/main/java/com/estetica/api_estetica/repository/UserRepository.java
+++ src/main/java/com/estetica/api_estetica/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.estetica.api_estetica.repository;
 import com.estetica.api_estetica.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserEntityByUsername(String username);
 }

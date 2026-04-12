54c72a07c9c9708e01bbc843ba1087da378eaff7
devGonzaloCastellano <devgonzalocastellano@gmail.com>
2026-04-12 15:01:12 -0300

    feat: add PreAuthorize rules to TreatmentController

--- src/main/java/com/estetica/api_estetica/controller/TreatmentController.java
+++ src/main/java/com/estetica/api_estetica/controller/TreatmentController.java
@@ -7,12 +7,14 @@ import com.estetica.api_estetica.service.ITreatmentService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
+@PreAuthorize("denyAll()")
 @RequestMapping("/api/treatments")
 public class TreatmentController {
 
@@ -20,11 +22,13 @@ public class TreatmentController {
     private ITreatmentService treatmentService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE', 'CLIENT')")
     public ResponseEntity<List<TreatmentResponseDTO>> getTreatment(){
         return ResponseEntity.ok(treatmentService.getTreatment());
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<TreatmentResponseDTO> createTreatment(@Valid @RequestBody TreatmentCreateDTO dto){
 
         TreatmentResponseDTO treatmentCreado = treatmentService.createTreatment(dto);
@@ -35,11 +39,13 @@ public class TreatmentController {
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<TreatmentResponseDTO> updateTreatment(@PathVariable Long id, @RequestBody TreatmentUpdateDTO dto){
         return ResponseEntity.ok(treatmentService.updateTreatment(id, dto));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteTreatment(@PathVariable Long id){
         treatmentService.deleteTreatment(id);
         return ResponseEntity.noContent().build();

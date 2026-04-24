b05456290eb48d1081c7ce5ea6a063c1046428f1
devGonzaloCastellano <devgonzalocastellano@gmail.com>
2026-04-24 11:28:05 -0300

    refactor(User): remove redundant nullable=true from email and username columns

--- src/main/java/com/estetica/api_estetica/model/entity/User.java
+++ src/main/java/com/estetica/api_estetica/model/entity/User.java
@@ -21,10 +21,10 @@ public class User {
     private String firstname;
     private String lastname;
 
-    @Column(unique = true, nullable = true)
+    @Column(unique = true)
     private String email;
 
-    @Column(unique = true, nullable = true)
+    @Column(unique = true)
     private String username;
     private String password;
 
